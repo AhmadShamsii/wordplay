bab6a1e03a28afda12e378a546eeff32c2efe5ca
Ahmad Shamsi <ahmaddshamsii@gmail.com>
2023-11-10 12:48:08 +0500

    fix: removed some bugs

--- src/containers/GamePage/index.tsx
+++ src/containers/GamePage/index.tsx
@@ -110,7 +110,6 @@ const GamePage = () => {
     }
     gamesCounter();
     // Update the state with the new values
-    //minor changes
     setScoreData({
       totalGames,
       totalWords: newTotalWordsValue,
