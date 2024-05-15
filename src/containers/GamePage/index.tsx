70b254613610ced88ee80b8e6a0da3915eb4af35
Ahmad Shamsi <ahmaddshamsii@gmail.com>
2024-05-16 00:21:59 +0500

    fix: still debugging

--- src/containers/GamePage/index.tsx
+++ src/containers/GamePage/index.tsx
@@ -61,6 +61,7 @@ const GamePage = () => {
   };
 
   console.log('do protetive routing')
+  console.log('do protetive routing')
 
 // clear the score and random letter when component mounts
   useEffect(() => {
